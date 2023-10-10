85e9c5fe7fdaa412f0adf45a6a06a4cd33f0413e
chimcuccu258 <vuminhnga25@gmail.com>
2023-10-10 09:16:14 +0700

    initial commit

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Nga Vũ on 10/10/2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Second Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MeetingView()
+}
