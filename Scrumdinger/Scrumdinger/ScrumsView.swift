ae92c66441f684daeedd10d92df198af122e627c
chimcuccu258 <vuminhnga25@gmail.com>
2023-10-10 10:58:36 +0700

    navigation-hierarchy

---
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,40 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Nga Vũ on 10/10/2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                //                CardView(scrum: scrum)
+                //                .listRowBackground(scrum.theme.mainColor)
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+            }
+            .navigationTitle("Daily Scrum")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+    }
+}
+
+//#Preview {
+//    ScrumsView()
+//}
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
